0952928987d65e16735c63831069691d3251c6d3
Abdallah317 <80751311+Abdallah317@users.noreply.github.com>
2025-07-01 10:10:43 +0300

    Add files via upload

---
+++ Data_Structures/Stack_Dynamic_DS_Based_On_Array/c files/PLATFORM.c
@@ -0,0 +1,72 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+#define CPU_TYPE_8  8
+#define CPU_TYPE_16 16
+#define CPU_TYPE_32 32
+#define CPU_TYPE_64 64
+/*************************************/
+#define CPU_TYPE (CPU_TYPE_64)
+/*************************************/
+typedef enum return_status{
+
+R_NOK,  //0
+R_OK    //1
+
+}return_status_t;
+#if (CPU_TYPE == CPU_TYPE_64)
+typedef unsigned char boolean;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed int sint32;
+typedef signed long long sint64;
+typedef float float32;
+typedef double float64;
+
+#elif (CPU_TYPE == CPU_TYPE_32)
+typedef unsigned char boolean;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+typedef unsigned long long uint64;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed long sint32;
+typedef signed long long sint64;
+typedef float float32;
+typedef double float64;
+
+#elif (CPU_TYPE == CPU_TYPE_16)
+typedef unsigned char boolean;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+typedef unsigned long long uint64;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed long sint32;
+typedef signed long long sint64;
+typedef float float32;
+typedef double float64;
+
+#elif (CPU_TYPE == CPU_TYPE_8)
+typedef unsigned char boolean;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed long sint32;
+typedef float float32;
+
+typedef unsigned int uint32;
+typedef unsigned short uint16;
+typedef unsigned char uint8;
+typedef signed int sint32;
+typedef signed short sint16;
+typedef signed char sint8;
+#endif // for if
+#endif // PLATFORM_H
