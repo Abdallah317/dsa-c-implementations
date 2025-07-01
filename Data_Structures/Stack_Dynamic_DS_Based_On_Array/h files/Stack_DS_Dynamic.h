30e8622b049ee3bba6398affc2542cbcc7bff4de
Abdallah317 <80751311+Abdallah317@users.noreply.github.com>
2025-07-01 10:11:42 +0300

    Add files via upload

---
+++ Data_Structures/Stack_Dynamic_DS_Based_On_Array/h files/Stack_DS_Dynamic.h
@@ -0,0 +1,16 @@
+#ifndef STACK_DS_DYNAMIC_H
+#define STACK_DS_DYNAMIC_H
+
+
+class Stack_DS_Dynamic
+{
+    public:
+        Stack_DS_Dynamic();
+        virtual ~Stack_DS_Dynamic();
+
+    protected:
+
+    private:
+};
+
+#endif // STACK_DS_DYNAMIC_H
